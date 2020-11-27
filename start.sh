df4e8959a5ef8e2e50aebc6befd72f92c53ded16
Ibadnf <ibaddilahnf@gmail.com>
2020-11-27 11:31:11 +0700

    2711

--- start.sh
+++ start.sh
@@ -1,2 +1,8 @@
-export APP_CONFIG_FILE='config.development.Development'
+export APP_CONFIG_FILE=
+export DB_HOST=
+export DB_PORT=
+export DB_USERNAME=
+export DB_NAME=
+export DB_PASSWORD=
+
 python run.py
