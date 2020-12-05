1d9b55bc87a686ce2a9ad6b895e94eac8fd9b241
Ibadnf <ibaddilahnf@gmail.com>
2020-12-05 22:13:18 +0700

    Malming Ngoding

--- app/views/main.py
+++ app/views/main.py
@@ -6,9 +6,31 @@ main = Blueprint('main', __name__)
 login_manager = LoginManager()
 
 @main.route('/dashboard')
+@login_required
 def dashboard():
-    if not current_user.is_authenticated:
-    	    flash('Please login!', 'danger')
-    	    return redirect(url_for('auth.login'))
-    
     return render_template('dashboard.html')
+
+@main.route('/poliklinik')
+@login_required
+def poliklinik():
+    return render_template('/poliklinik/poliklinik.html')
+
+@main.route('/tambahpoliklinik')
+@login_required
+def create_poli():
+    return render_template('/poliklinik/create_poli.html')
+
+@main.route('/updatepoliklinik')
+@login_required
+def update_poli():
+    return render_template('/poliklinik/update_poli.html')
+
+@main.route('/deletepoliklinik')
+@login_required
+def delete_poli():
+    return redirect(url_for('main.poliklinik'))
+
+@main.route('/dokter')
+@login_required
+def dokter():
+    return render_template('/dokter/dokter.html')
